bacf278db629793bbd3c278e2508521352378b93
raimonizard <97727989+raimonizard@users.noreply.github.com>
2025-09-30 17:25:06 +0200

    chore: update MainMotorbike.py

--- Class and inheritance/MainMotorbike.py
+++ Class and inheritance/MainMotorbike.py
@@ -1,9 +1,10 @@
 from Motorbike import *
 from MotoTrial import *
 
-motomami = Motorbike("Kawasaki", "Ninja", 100, 1000, 2023)
+motomami: Motorbike = Motorbike("Kawasaki", "Ninja", 100, 1000, 2023)
 
-motobou = MotoTrial("Montesa", "Cota", 300, 30, 2021, 19)
+motobou: MotoTrial = MotoTrial("Montesa", "Cota", 300, 30, 2021, 19)
 
 print(motomami)
+
 print(motobou)
